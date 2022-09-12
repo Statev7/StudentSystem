8113b9c062119a1cff8dd5d9c0c99f8039c0b68d
Statev07 <petiostatev2703@gmail.com>
2022-09-12 23:01:08 +0300

    Implement course controller.

---
+++ src/Web/StudentSystem.Web/Controllers/CoursesController.cs
@@ -0,0 +1,44 @@
+﻿namespace StudentSystem.Web.Controllers
+{
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Mvc;
+
+    using StudentSystem.Services.Course;
+    using StudentSystem.ViewModels.Course;
+
+    public class CoursesController : Controller
+    {
+        private readonly ICourseService courseService;
+
+        public CoursesController(ICourseService courseService)
+        {
+            this.courseService = courseService;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var courses = this.courseService.GetAll();
+
+            return View(courses);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+            => this.View();
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateCourseBindingModel course)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                return this.View(course);
+            }
+
+            await this.courseService.CreateAsync(course);
+
+            return this.RedirectToAction("Index");
+        }
+    }
+}

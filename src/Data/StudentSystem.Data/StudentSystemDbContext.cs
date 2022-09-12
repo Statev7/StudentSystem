1b07b97578de1af12c6ad774cc90b0d5812cb3be
Statev07 <petiostatev2703@gmail.com>
2022-09-12 21:28:53 +0300

    Create mvc project.

---
+++ src/Data/StudentSystem.Data/StudentSystemDbContext.cs
@@ -0,0 +1,13 @@
+﻿namespace StudentSystem.Web.Data
+{
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+
+    public class StudentSystemDbContext : IdentityDbContext
+    {
+        public StudentSystemDbContext(DbContextOptions<StudentSystemDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}

fe9aa6c7ee6ee4578a8f991cad84c20fdf7dd0de
Statev07 <petiostatev2703@gmail.com>
2022-09-14 12:41:26 +0300

    Add custom attribute.

---
+++ src/Web/StudentSystem.Web.Infrastructure/StudentSystem.Web.Infrastructure/Attributes/IsBeforeUtcNowAttribute.cs
@@ -0,0 +1,25 @@
+﻿namespace StudentSystem.Web.Infrastructure.Attributes
+{
+	using System;
+	using System.ComponentModel.DataAnnotations;
+	using System.Globalization;
+
+	[AttributeUsage(AttributeTargets.Property)]
+    public class IsBeforeUtcNowAttribute : ValidationAttribute
+    {
+		public override bool IsValid(object value)
+		{
+			DateTime valuesAsDateTime = (DateTime)value;
+
+			return valuesAsDateTime >= DateTime.UtcNow;
+        }
+
+		public override string FormatErrorMessage(string name)
+		{
+			var dateAsString = DateTime.UtcNow.Date.ToString("dd/M/yyyy", CultureInfo.InvariantCulture);
+			var errorMessage = $"The '{name}' cannot be earlier than {dateAsString}!";
+
+			return errorMessage;
+		}
+	}
+}

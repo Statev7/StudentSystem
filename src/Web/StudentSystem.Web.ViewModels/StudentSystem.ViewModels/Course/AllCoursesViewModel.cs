9461489036f4182c1c9eb2705b804f6db9f7d826
Statev07 <petiostatev2703@gmail.com>
2022-09-14 19:03:25 +0300

    Export date format logic.

--- src/Web/StudentSystem.Web.ViewModels/StudentSystem.ViewModels/Course/AllCoursesViewModel.cs
+++ src/Web/StudentSystem.Web.ViewModels/StudentSystem.ViewModels/Course/AllCoursesViewModel.cs
@@ -10,14 +10,9 @@
 
         public string ImageURL { get; set; }
 
-        public DateTime StartDate { get; set; }
+        public string StartDate { get; set; }
 
-        public DateTime EndDate { get; set; }
-
-        public double Duration
-            => this.CalculateDuration();
-
-        private double CalculateDuration()
-            => (EndDate - StartDate).TotalDays / 7 < 1 ? 1 : Math.Ceiling((EndDate - StartDate).TotalDays / 7);
+        //From automapper.
+        public double Duration { get; set; }
     }
 }
--- src/Web/StudentSystem.Web/Infrastructure/MappingConfiguration/CourseProfile.cs
+++ src/Web/StudentSystem.Web/Infrastructure/MappingConfiguration/CourseProfile.cs
@@ -1,5 +1,8 @@
 ﻿namespace StudentSystem.Web.Infrastructure.MappingConfiguration
 {
+    using System;
+    using System.Globalization;
+
     using AutoMapper;
 
     using StudentSystem.Data.Models.StudentSystem;
@@ -9,7 +12,14 @@
     {
         public CourseProfile()
         {
-            this.CreateMap<Course, AllCoursesViewModel>();
+            this.CreateMap<Course, AllCoursesViewModel>()
+                .ForMember(d => d.StartDate, conf => conf
+                    .MapFrom(s => s.StartDate.ToString("d-MMMM yyyy", CultureInfo.InvariantCulture)))
+                .ForMember(d => d.Duration, conf => conf
+                    .MapFrom(s => (s.EndDate - s.StartDate).TotalDays / 7 < 1 
+                            ? 1 
+                            : Math.Ceiling((s.EndDate - s.StartDate).TotalDays / 7)));
+
             this.CreateMap<CreateCourseBindingModel, Course>();
             this.CreateMap<Course, DetailCourseViewModel>();
             this.CreateMap<UpdateCourseBindingModel, Course>().ReverseMap();
